c6714f41adeec598535ccf0b6db054e4be01c55a
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-06-28 20:16:40 +0300

    Update Controller.cs

--- Controller.cs
+++ Controller.cs
@@ -1,18 +1,23 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Web;
 using System.Linq;
+using System.Net.Http;
+using HtmlAgilityPack;
 using Shared.Engine.CORE;
 using Lampac.Engine;
 using Shared.Model.Templates;
 using Uaflix.Models.UaFlix;
+using System.Text.RegularExpressions;
 
 namespace Uaflix.Controllers
 {
-    public class UaFlix : BaseController
+    public class Controller : BaseController
     {
         ProxyManager proxyManager = new ProxyManager(ModInit.UaFlix);
+        static HttpClient httpClient = new HttpClient();
 
         [HttpGet]
         [Route("uaflix")]
@@ -23,208 +28,168 @@ namespace Uaflix.Controllers
                 return Forbid();
 
             var proxy = proxyManager.Get();
+            var result = await search(imdb_id, kinopoisk_id, title, original_title, serial);
 
-            var result = await search(imdb_id, kinopoisk_id, serial);
             if (result == null)
             {
                 proxyManager.Refresh();
-                return Ok();
+                return Content("Uaflix", "text/html; charset=utf-8");
             }
 
             if (result.movie != null)
             {
-                var tpl = new MovieTpl(title, original_title);
+                var tpl = new MovieTpl(title, original_title, result.movie.Count);
 
                 foreach (var movie in result.movie)
                 {
                     var streamquality = new StreamQualityTpl();
                     foreach (var item in movie.links)
-                        streamquality.Append(HostStreamProxy(init, item.link, proxy: proxy), item.quality);
+                        streamquality.Append(HostStreamProxy(ModInit.UaFlix, item.link, proxy: proxy), item.quality);
 
-                    tpl.Append(movie.translation, streamquality.Firts().link, quality: streamquality.Firts().quality, streamquality: streamquality);
+                    tpl.Append(
+                        movie.translation,
+                        streamquality.Firts().link,
+                        streamquality: streamquality,
+                        subtitles: movie.subtitles
+                    );
                 }
 
-                if (rjson)
-                    return Content(tpl.ToJson(), "application/json; charset=utf-8");
-
-                return Content(tpl.ToHtml(), "text/html; charset=utf-8");
+                return rjson
+                    ? Content(tpl.ToJson(), "application/json; charset=utf-8")
+                    : Content(tpl.ToHtml(), "text/html; charset=utf-8");
             }
-            else
-            {
-                if (result.serial == null)
-                {
-                    proxyManager.Refresh();
-                    return Ok();
-                }
 
-                string defaultargs = $"&imdb_id={imdb_id}&kinopoisk_id={kinopoisk_id}&title={HttpUtility.UrlEncode(title)}&original_title={HttpUtility.UrlEncode(original_title)}&serial={serial}";
-
-                if (s == -1)
-                {
-                    var tpl = new SeasonTpl(quality: "4K HDR");
-
-                    foreach (var season in result.serial)
-                        tpl.Append($"{season.Key} сезон", $"{host}/uaflix?s={season.Key}" + defaultargs, season.Key);
-
-                    if (rjson)
-                        return Content(tpl.ToJson(), "application/json; charset=utf-8");
-
-                    return Content(tpl.ToHtml(), "text/html; charset=utf-8");
-                }
-                else
-                {
-                    var vtpl = new VoiceTpl();
-                    var etpl = new EpisodeTpl();
-
-                    string activTranslate = t;
-
-                    foreach (var translation in result.serial[s.ToString()])
-                    {
-                        if (string.IsNullOrEmpty(activTranslate))
-                            activTranslate = translation.id;
-
-                        vtpl.Append(translation.name, activTranslate == translation.id, $"{host}/uaflix?s={s}&t={translation.id}" + defaultargs);
-                    }
-
-                    foreach (var episode in result.serial[s.ToString()].First(i => i.id == activTranslate).episodes)
-                    {
-                        var streamquality = new StreamQualityTpl();
-                        foreach (var item in episode.links)
-                            streamquality.Append(HostStreamProxy(init, item.link, proxy: proxy), item.quality);
-
-                        etpl.Append($"{episode.id} серия", $"{title ?? original_title} ({episode.id} серия)", s.ToString(), episode.id, streamquality.Firts().link, streamquality: streamquality);
-                    }
-
-                    if (rjson)
-                        return Content(etpl.ToJson(vtpl), "application/json; charset=utf-8");
-
-                    return Content(vtpl.ToHtml() + etpl.ToHtml(), "text/html; charset=utf-8");
-                }
-            }
+            return Content("Uaflix", "text/html; charset=utf-8");
         }
 
-
-        async ValueTask<Result> search(string imdb_id, long kinopoisk_id, int serial)
+        async ValueTask<Result> search(string imdb_id, long kinopoisk_id, string title, string original_title, int serial)
         {
             string memKey = $"UaFlix:view:{kinopoisk_id}:{imdb_id}";
             if (!hybridCache.TryGetValue(memKey, out Result res))
             {
-                await Task.Delay(1000); // имитация поиска
+                try
+                {
+                    string filmTitle = !string.IsNullOrEmpty(title) ? title : original_title;
+                    string searchUrl = $"https://uafix.net/index.php?do=search&subaction=search&story={HttpUtility.UrlEncode(filmTitle)}";
 
-                var defaultLinks = new List<(string link, string quality)>()
-                {
-                    ("https://www.elecard.com/storage/video/TheaterSquare_3840x2160.mp4", "2160p"),
-                    ("https://www.elecard.com/storage/video/TheaterSquare_1920x1080.mp4", "1080p"),
-                    ("https://www.elecard.com/storage/video/TheaterSquare_1280x720.mp4", "720p")
-                };
+                    httpClient.DefaultRequestHeaders.Clear();
+                    httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
+                    httpClient.DefaultRequestHeaders.Add("Referer", "https://uafix.net/");
 
-                if (serial == 0)
-                {
-                    res = new Result()
+                    var searchHtml = await httpClient.GetStringAsync(searchUrl);
+                    var doc = new HtmlDocument();
+                    doc.LoadHtml(searchHtml);
+
+                    var filmNode = doc.DocumentNode.SelectSingleNode("//a[contains(@class, 'sres-wrap')]");
+                    if (filmNode == null) return null;
+
+                    string filmUrl = filmNode.GetAttributeValue("href", "");
+                    if (!filmUrl.StartsWith("http"))
+                        filmUrl = "https://uafix.net" + filmUrl;
+
+                    var filmHtml = await httpClient.GetStringAsync(filmUrl);
+                    doc.LoadHtml(filmHtml);
+
+                    var iframeNodes = doc.DocumentNode.SelectNodes("//div[contains(@class, 'video-box')]/iframe");
+                    if (iframeNodes == null || !iframeNodes.Any()) return null;
+
+                    var movies = new List<Movie>();
+
+                    foreach (var iframe in iframeNodes)
                     {
-                        movie = new List<Movie>()
-                        { 
-                            new Movie()
-                            {
-                                translation = "RHS",
-                                links = defaultLinks
-                            },
-                            new Movie()
-                            {
-                                translation = "ViruseProject",
-                                links = defaultLinks
-                            }
-                        }
-                    };
-                }
-                else
-                {
-                    res = new Result()
-                    {
-                        serial = new Dictionary<string, List<Voice>>() 
+                        string iframeUrl = iframe.GetAttributeValue("src", "");
+                        if (string.IsNullOrEmpty(iframeUrl)) continue;
+
+                        if (iframeUrl.Contains("zetvideo.net"))
                         {
-                            ["1"] = new List<Voice>() 
-                            {
-                                new Voice()
+                            var zlinks = await ParseAllZetvideoSources(iframeUrl);
+                            foreach (var l in zlinks)
+                                movies.Add(new Movie
                                 {
-                                    id = "36",
-                                    name = "ViruseProject",
-                                    episodes = new List<Serial> 
-                                    {
-                                        new Serial() 
-                                        { 
-                                            id = "1",
-                                            links = defaultLinks
-                                        },
-                                        new Serial()
-                                        {
-                                            id = "2",
-                                            links = defaultLinks
-                                        },
-                                        new Serial()
-                                        {
-                                            id = "3",
-                                            links = defaultLinks
-                                        }
-                                    }
-                                },
-                                new Voice()
-                                {
-                                    id = "12",
-                                    name = "RHS",
-                                    episodes = new List<Serial>
-                                    {
-                                        new Serial()
-                                        {
-                                            id = "1",
-                                            links = defaultLinks
-                                        },
-                                        new Serial()
-                                        {
-                                            id = "2",
-                                            links = defaultLinks
-                                        }
-                                    }
-                                }
-                            },
-                            ["2"] = new List<Voice>()
-                            {
-                                new Voice()
-                                {
-                                    id = "36",
-                                    name = "ViruseProject",
-                                    episodes = new List<Serial>
-                                    {
-                                        new Serial()
-                                        {
-                                            id = "1",
-                                            links = defaultLinks
-                                        }
-                                    }
-                                },
-                                new Voice()
-                                {
-                                    id = "12",
-                                    name = "RHS",
-                                    episodes = new List<Serial>
-                                    {
-                                        new Serial()
-                                        {
-                                            id = "1",
-                                            links = defaultLinks
-                                        }
-                                    }
-                                }
-                            }
+                                    translation = $"{filmTitle} (Zetvideo)",
+                                    links = new List<(string, string)> { (l.link, l.quality) },
+                                    subtitles = null
+                                });
                         }
-                    };
+                    }
+
+                    if (movies.Count > 0)
+                    {
+                        res = new Result()
+                        {
+                            movie = movies
+                        };
+                        hybridCache.Set(memKey, res, cacheTime(5));
+                        proxyManager.Success();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"UaFlix error: {ex.Message}");
+                }
+            }
+            return res;
+        }
+
+        async Task<List<(string link, string quality)>> ParseAllZetvideoSources(string iframeUrl)
+        {
+            var result = new List<(string link, string quality)>();
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, iframeUrl);
+                request.Headers.Add("User-Agent", "Mozilla/5.0");
+                var response = await httpClient.SendAsync(request);
+                var html = await response.Content.ReadAsStringAsync();
+                var doc = new HtmlDocument();
+                doc.LoadHtml(html);
+
+                var sourceNodes = doc.DocumentNode.SelectNodes("//source[contains(@src, '.m3u8')]");
+                if (sourceNodes != null)
+                {
+                    foreach (var node in sourceNodes)
+                    {
+                        var url = node.GetAttributeValue("src", null);
+                        var label = node.GetAttributeValue("label", null) ?? node.GetAttributeValue("res", null) ?? "1080p";
+                        if (!string.IsNullOrEmpty(url))
+                            result.Add((url, label));
+                    }
                 }
 
-                proxyManager.Success();
-                hybridCache.Set(memKey, res, cacheTime(5));
+                if (result.Count == 0)
+                {
+                    var scriptNodes = doc.DocumentNode.SelectNodes("//script");
+                    if (scriptNodes != null)
+                    {
+                        foreach (var script in scriptNodes)
+                        {
+                            var text = script.InnerText;
+                            var urls = Regex.Matches(text, @"https?:\/\/[^\s'""]+\.m3u8")
+                                .Cast<Match>()
+                                .Select(m => m.Value)
+                                .Distinct();
+                            foreach (var url in urls)
+                                result.Add((url, "1080p"));
+                        }
+                    }
+                }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Zetvideo parse error: {ex.Message}");
+            }
+            return result;
+        }
 
-            return res;
+        public class Movie
+        {
+            public string translation { get; set; }
+            public List<(string link, string quality)> links { get; set; }
+            public SubtitleTpl? subtitles { get; set; }
+        }
+
+        public class Result
+        {
+            public List<Movie> movie { get; set; }
         }
     }
 }
